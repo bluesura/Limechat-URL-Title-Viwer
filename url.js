53cf6735dd1b4a1ceecbc8cff4d8a6230c0af270
bluesura <you@example.com>
2015-10-01 22:22:28 +0900

    v1.2

--- url.js
+++ url.js
@@ -1,11 +1,15 @@
-﻿//2011/12/05
-//eval関数いい加減使うのあれなんで廃止
+﻿//2011/12/06
+// byte計算方法とか微調整
+//2011/12/05
+// eval関数いい加減使うのあれなんで廃止
 // unescapeHtmlCharacterの量が多すぎたので最小限に
 // HEADでいったん安全性？確認する
+// byteを表示するようにしてみた
+// utf-8別に変換し直さなくていいんじゃね？
 /**
 	@description LimechatでのURL解析用スクリプト.
 	@author sura.
-	@version ｖ1.1.
+	@version ｖ1.2.
 	@since 2011/08/21.
  */
 
@@ -16,9 +20,8 @@
 	@param {String} _text 発言が入れられている.
  */
 function event::onChannelText(_prefix, _channel, _text) {
-	if (!/\[(マクロ|スクリプト|macro|script)\]/i.test(_text))
-		if (/(https?:\/\/[\w-~+*_@.,';:!?$&=%#()\/]+)/i.exec(_text))
-			getHeader(_channel, convertUrl(RegExp.$1));
+	if (/(https?:\/\/[\w-~+*_@.,';:!?$&=%#()\/]+)/i.exec(_text))
+		getHeader(_channel, convertUrl(RegExp.$1));
 }
 
 /**
@@ -27,12 +30,11 @@ function event::onChannelText(_prefix, _channel, _text) {
 	@return {String} 変換されたか・されてないURL.
  */
 function convertUrl(_url) {
-	_url = _url.replace(/#\w+$/, '');/*アンカー回避*/
 	if (/^http:\/\/(?:www.nicovideo.jp\/watch|nico\.ms)\/((?:[sn]m)?\d+)/.exec(_url))
 		return 'http://ext.nicovideo.jp/api/getthumbinfo/' + RegExp.$1;
 	else if (/^http:\/\/www.youtube.com\/watch?.*?v=([\-\w]+)/.exec(_url))
 		return 'http://gdata.youtube.com/feeds/api/videos/' + RegExp.$1;
-	return _url;
+	return _url.replace(/#\w+$/, '');/*アンカー回避*/
 }
 
 /**
@@ -45,8 +47,10 @@ function getHeader(_channel, _url) {
 	axo.onreadystatechange = function() {
 		if (axo.readyState == 4) {
 			try {
-				if (/(text\/(\w+?)|application\/(atom+xml|atomcat+xml|atomsvc+xml))/.exec(axo.getResponseHeader('Content-Type')) || axo.getResponseHeader('Content-Length') < 1000000) {
+				if (/(text\/\w+|application\/atom(cat|svc)?\+xml)/.exec(axo.getResponseHeader('Content-Type'))) {
 					getHTTP(_channel, _url);
+				} else if (axo.getResponseHeader('Content-Length')) {
+					send(_channel, '<color>07[script]<color>Filesize:' + getStringFilesize(axo.getResponseHeader('Content-Length')));
 				}
 			} catch (e) {} finally {
 				axo.onreadystatechange = new Function();/*メモリリーク回避*/
@@ -54,7 +58,25 @@ function getHeader(_channel, _url) {
 		}
 	}
 	axo.open('HEAD', _url, true);
-	axo.send(null);
+	axo.send('');
+}
+
+/**
+	@description 数値をbyte単位にして返します.
+	@param {Number} _byte 変換したい数値.
+	@return {Number} 変換された数値.
+ */
+function getStringFilesize(_byte) {
+	if (_byte > 1023) {
+		if (_byte > 1048575) {
+				if (_byte > 1073741823) {
+					return (_byte/1073741824|0) + 'GB';
+				}
+			return (_byte/1048576|0) + 'MB';
+		}
+		return (_byte/1024|0) + 'KB';
+	}
+	return (_byte|0) + 'Byte';
 }
 
 /**
@@ -74,7 +96,7 @@ function getHTTP(_channel, _url) {
 		}
 	}
 	axo.open('GET', _url, true);
-	axo.send(null);
+	axo.send('');
 }
 
 /**
@@ -84,27 +106,24 @@ function getHTTP(_channel, _url) {
  */
 function encodeCharset(_axo) {
 	var stream = new ActiveXObject('ADODB.Stream');
+	try {
+		var charset = '_autodetect';
+		if (_axo.getResponseHeader('Content-Type').match(/charset=["']?([\w-]+)/i)) {
+			charset = RegExp.$1;
+		} else if (_axo.responseText.match(/<head>(?:.|\n)*?charset=["']?([\w-_]+)(?:.|\n)*?<\/head>/i)) {
+			charset = RegExp.$1;
+		}
+	} catch (e) {
+		charset = 'Shift_JIS';
+	} finally {
+		if (charset == 'utf-8') { return val; }
+		stream.Charset = charset;
+	}
 	stream.Open();
 	stream.Type = 1;
 	stream.Write(_axo.responseBody);
 	stream.Position = 0;
 	stream.Type = 2;
-	try {
-		var val = _axo.responseText;
-		var charset = '';
-		var contentType = _axo.getResponseHeader('Content-Type');
-		if (contentType.match(/charset=["']?([\w-]+)/i)) {
-			charset = RegExp.$1;
-		} else if (val.match(/<head>(?:.|\n)*?charset=["']?([\w-]+)(?:.|\n)*?<\/head>/i)) {
-			charset = RegExp.$1;
-		} else {
-			charset = '_autodetect';
-		}
-	} catch (e) {
-		charset = 'Shift_JIS';
-	} finally {
-		stream.Charset = charset;
-	}
 	try {
 		var text = '';
 		text = stream.ReadText();
@@ -127,61 +146,48 @@ function checkUrl(_url, _text) {
 	return '';
 }
 
-	var hosts = {
-	'ext.nicovideo.jp/api/getthumbinfo/': function (_text) {
-		try {
-			return (parseTitle(_text) + '(' + /<length>(.*?)<\/length>/i.exec(_text)[1] + ')' + ' <color>12[説明]<color> ' + /<description>(.*?)<\/description>/i.exec(_text)[1]);
-		} catch (e) {try {
-			return (/<code>(.*?)<\/code>/.exec(_text)[1]);
-		} catch (e) { return parseTitle(_text)}}
-	},
-	'gdata.youtube.com/feeds/api/videos/': function (_text) {
-		try {
-			return (parseTitle(_text) + ' <color>12[説明]<color> ' + /<content type='text'>(.*?)<\/content>/i.exec(_text)[1]);
-		} catch (e) {return parseTitle(_text)}
-	},
-	'www.pixiv.net/.*illust_id=': function (_text) {
-		try {
-			return (/<title>「(.*?)」\//.exec(_text)[1] + ' <color>12[user]<color> ' + /\/「(.*?)」の.*?<\/title>/.exec(_text)[1] + ' <color>12[説明]<color> ' + /<meta property="og:description" content="(.*?)">/.exec(_text)[1]);
-		} catch (e) {return parseTitle(_text)}
-	},
-	'www1.axfc.net': function (_text) {
-	try {
-		var temp = '' + parseTitle(_text);
+var hosts = {
+	/*専用解析*/
+	'ext.nicovideo.jp/api/getthumbinfo/': function (_text) {try {
+		return (hosts['*'](_text) + '(' + /<length>(.*?)<\/length>/i.exec(_text)[1] + ')' + ' <color>12[説明]<color> ' + /<description>(.*?)<\/description>/i.exec(_text)[1]);
+	} catch (e) {try {
+		return (/<code>(.*?)<\/code>/.exec(_text)[1]);
+	} catch (e) { return hosts['*'](_text)}}},
+	'gdata.youtube.com/feeds/api/videos/': function (_text) {try {
+		return (hosts['*'](_text) + ' <color>12[説明]<color> ' + /<content type='text'>(.*?)<\/content>/i.exec(_text)[1]);
+	} catch (e) {return hosts['*'](_text)}},
+	'www.pixiv.net/.*illust_id=': function (_text) {try {
+		return (/<title>「(.*?)」\//.exec(_text)[1] + ' <color>12[user]<color> ' + /\/「(.*?)」の.*?<\/title>/.exec(_text)[1] + ' <color>12[説明]<color> ' + /<meta property="og:description" content="(.*?)">/.exec(_text)[1]);
+	} catch (e) {return hosts['*'](_text)}},
+	'www1.axfc.net': function (_text) {try {
+		var temp = '' + hosts['*'](_text);
 		if (/<div class="comme"><h3>投稿者ファイル説明<\/h3><p>(.*?)<br><\/p>/.exec(_text))
 			temp += ' <color>12[説明]<color> ' + RegExp.$1;
 		if (/<h3>オリジナルファイルネーム<\/h3><p>(.*?)<\/p><\/div>/.exec(_text))
 			temp += ' <color>12[file]<color> ' + RegExp.$1;
 		return temp;
-	} catch (e) {return parseTitle(_text)}
-},
-	'u6.getuploader.com': function (_text) {
-		try {
-			return (parseTitle(_text) + ' | ' + /<meta name="Description" content="(.*?)"/.exec(_text)[1]);
-		} catch (e) {return parseTitle(_text)}
-	},
+	} catch (e) {return hosts['*'](_text)}},
+	'u6.getuploader.com': function (_text) {try {
+		return (hosts['*'](_text) + ' | ' + /<meta name="Description" content="(.*?)"/.exec(_text)[1]);
+	} catch (e) {return hosts['*'](_text)}},
 	'twitvideo.jp': function (_text) {try {
-	return (parseTitle(_text) + ' <color>12[コメント]<color> ' + /<span class="sf_comment">(.*?)<\/span>/.exec(_text)[1]);
-} catch (e) {return parseTitle(_text)}}
-,
+		return (hosts['*'](_text) + ' <color>12[コメント]<color> ' + /<span class="sf_comment">(.*?)<\/span>/.exec(_text)[1]);
+	} catch (e) {return hosts['*'](_text)}},
 	'twitpic.com': function (_text) {try {
-		return (parseTitle(_text) + ' <color>12[名前]<color> ' + /<p><span id="photo-info-name">(.*?)<\/span>/.exec(_text)[1]);
-	} catch (e) {return parseTitle(_text)}
-},
+		return (hosts['*'](_text) + ' <color>12[名前]<color> ' + /<p><span id="photo-info-name">(.*?)<\/span>/.exec(_text)[1]);
+	} catch (e) {return hosts['*'](_text)}},
 /**
 	@description タイトルのタグを調べて返します.
 	@param {String} _text GETしてきた内容.
 	@return {String} 解析結果.
  */
-	'*': function parseTitle(_text) {
-		try {
-			return (/<title.*?>((?:.|\n|\r)*?)<\/title>/i.exec(_text)[1]);
-		} catch (e) {return;}
-	}
+	'*': function (_text) {try {
+		return (/<title.*?>((?:.|\n|\r)*?)<\/title>/i.exec(_text)[1]);
+	} catch (e) {return;}}
 };
 
 /**
-	@description 余分なスペースを削除したり、一定の長さに整える.
+	@description 余分なスペース・改行を削除したり、一定の長さに整える.
 	@param {String} _text 整理する文字列.
 	@return {String} 整理された文字列.
  */
